224ba965d6e2e82f27f7694865b043de58923a08
Rohen <mailme.rohen@gmail.com>
2016-08-29 18:11:57 +0400

    Secant Method for finding roots of Nonlinear Equations

---
+++ Secant_Method.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+import matplotlib.pyplot
+
+def f(x):
+  return (x - 1) * (x + 5.0)
+
+check = 10**-3 #used for precision checking
+
+for a in range(-10, 10):
+  for b in range(-a, a):
+    if (f(a) * f(b) < 0):
+      break
+    break
+
+while abs(b - a) > check:
+  c = b - ((f(b) * (b - a))) / (f(b) - f(a))
+  a = b
+  b = c
+
+print c, f(c)
